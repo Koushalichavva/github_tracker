f9d305582b0f6d648d7f6dee5bce79495acd0859
Koushalichavva <24b01a1218@svecw.edu.in>
2025-12-17 23:21:18 +0530

    Fix GitHub API auth and clean rebase state

--- github_tracker.py
+++ github_tracker.py
@@ -1,35 +1,9 @@
 import requests
-<<<<<<< HEAD
-
-def get_user_details(username):
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url)
-    return response.json()
-
-def get_repositories(username):
-    url = f"https://api.github.com/users/{username}/repos?sort=created&direction=asc"
-    response = requests.get(url)
-    return response.json()
-
-def get_commit_summary(username, repo_name):
-    url = f"https://api.github.com/repos/{username}/{repo_name}/commits"
-    response = requests.get(url)
-    commits = response.json()
-
-    if isinstance(commits, list) and len(commits) > 0:
-        commit_count = len(commits)
-        last_commit_date = commits[0]["commit"]["author"]["date"]  # latest in this repo
-    else:
-        commit_count = 0
-        last_commit_date = "No commits"
-
-    return commit_count, last_commit_date
-=======
 import time
 import os
 from dotenv import load_dotenv
-load_dotenv()   # loads variables from .env into environment
-GITHUB_TOKEN = os.getenv("MY_GITHUB_TOKEN")
+load_dotenv()
+GITHUB_TOKEN=os.getenv("GITHUB_TOKEN")
 HEADERS = {
     "Authorization": f"token {GITHUB_TOKEN}"
 }
@@ -70,7 +44,6 @@ def get_commit_summary(username, repo_name):
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch commits for {repo_name}:", e)
         return 0, "No commits"
->>>>>>> adc7964 (new token added)
 
 def main():
     username = input("Enter GitHub username: ")
@@ -89,11 +62,8 @@ def main():
         if last_commit_date != "No commits":
             if (latest_commit_overall is None) or (last_commit_date > latest_commit_overall):
                 latest_commit_overall = last_commit_date
-<<<<<<< HEAD
-=======
         time.sleep(0.5)
->>>>>>> adc7964 (new token added)
     print("Last Commit Date (overall):", latest_commit_overall)
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
